1bf05d35d29714c738060b0230945986947b8acf
rojesh <rojace2011@gmail.com>
2016-11-04 12:26:48 +0545

    Initial commit

---
+++ imports/ui/body.js
@@ -0,0 +1,43 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import './body.html';
+import './task.js';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+Template.body.helpers({
+  tasks() {
+  	const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+});
